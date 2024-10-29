6b8579abb6dc2f473877062623bbe33fa90e40a2
vilker <francisco.lopes275@aluno.ce.gov.br>
2024-10-29 10:24:18 -0400

    inicialização da criação da pagina de detalhes do livro

--- public/livro.js
+++ public/livro.js
@@ -1,4 +1,13 @@
 // livro.js
+function espacos(texto) {
+    return texto.replace(/\+/g, ' ');
+}
+
+// Exemplo de uso
+const stringOriginal = "Este+é+um+exemplo+de+string+com+caracteres+de+mais.";
+const stringModificada = espacos(stringOriginal);
+
+console.log(stringModificada); // Saída: "Este é um exemplo de string com caracteres de mais."
 
 function getQueryParams() {
     const params = {};
@@ -17,11 +26,18 @@ window.onload = function() {
         var titulo =document.getElementById("titulo")
         var informacoes = document.getElementById("informacoes")
         var emprestimo =document.getElementById("emprestimo")
+        var Timagem = espacos(livroItem["imagem"])
+        var Tdescricao = espacos(livroItem["descricao"])
+        var Tgenero =espacos(livroItem["genero"])
 
-        imagem.innerHTML = `<img src="${livroItem["imagem"]}" alt="${livroItem["descricao"]}" />`
+        imagem.innerHTML = `<img src="${Timagem}" alt="" />`
         titulo.innerHTML= `<h3>Autor: ${livroItem["autor"]}</h3>`
-        informacoes.innerHTML =`<h2>Informações</h2><br>
-        <h3>Autor: ${livroItem["autor"]}</brsss`
+        informacoes.innerHTML =`<h2 id="titulo2">Informações
+        <h3>Autor: ${livroItem["autor"]}</h3>
+        <h3>Genero: ${livroItem["genero"]}</h3>
+        <br>
+        <h3 id="descricao">Descrição</h3>
+        <p id="textos">${Tdescricao}`
 
         
     } else {
